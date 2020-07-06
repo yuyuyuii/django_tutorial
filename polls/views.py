2582499833b8b5dc5841464cd3686966965c7b35
y.maeda <yuyuyu.mae606@gmail.com>
2020-07-06 15:50:35 +0900

    コンテンツの一覧表示

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,23 @@
+#httpとtemplateの読み込みをまとめて行ってくれる
 from django.shortcuts import render
-from django.http import HttpResponse # http通信を行う
+# from django.http import HttpResponse  # http通信を行う
+# from django.template import loader # templateの読み込みを行う
+from .models import Question # Questionモデルをインポート
 
 def index(request):
-  return HttpResponse('hello world')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+      'latest_question_list' :latest_question_list,
+    }
+    return render(request,'polls/index.html', context)
+
+def detail(request, question_id):
+    details = Question.objects.get(pk=question_id)
+    return render(request, 'polls/detail.html', {'details': details })
+
+def results(request, question_id):
+    response = "you're looking at the results of question %s."
+    return render(response % question_id)
+
+def vote(request, question_id):
+    return render("you're voting on question %s." % question_id)
